33177eff01771df2783b0a381ea6982e270b955a
preranans <85788590+preranans@users.noreply.github.com>
2021-09-22 21:11:19 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 	public static void main(String args[]) {
 		
            System.out.println("Hello World");
-           System.out.println("Hi,I am Nishanth");
+           System.out.println("Hi,I am Prerana");
 		}
 
 }
